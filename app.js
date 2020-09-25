ce7f0e6e3c4ec800e34094ff87ed20b85b7cf1e9
Jonathon Young <jono@jyoung.co>
2020-09-25 17:12:41 +1000

    feat: eslint integration
    ESLint and Prettier have both been integrated to perform formatting.

--- app.js
+++ app.js
@@ -7,43 +7,48 @@ var bodyParser = require('body-parser');
 var flash = require('express-flash');
 var mysql = require('mysql');
 var session = require('express-session');
-var mysql_store = require('express-mysql-session')(session);
+var mysqlStore = require('express-mysql-session')(session);
 
 // MySQL related code.
 var db = mysql.createConnection({
-    host: 'localhost',
-    port: 3306,
-    user: 'root',
-    password: '',
-    database: 'theta'
+  host: 'localhost',
+  port: 3306,
+  user: 'root',
+  password: '',
+  database: 'theta',
 });
 
 // Connect to the database.
 db.connect((err) => {
-    if (err) {
-        console.log('[Server] [DB]: Error connecting to MySQL: ' + err);
-        process.exit(1);
-    } else {
-	    console.log('[Server] [DB]: Successfully connected to MySQL.');
-    }
+  if (err) {
+    console.log('[Server] [DB]: Error connecting to MySQL: ' + err);
+    process.exit(1);
+  } else {
+    console.log('[Server] [DB]: Successfully connected to MySQL.');
+  }
 });
 
 // Setup the session store and make the database variables global.
-var sessionStore = new mysql_store({
-    expiration: 3600000
-}, db);
+var sessionStore = new mysqlStore(
+  {
+    expiration: 3600000,
+  },
+  db,
+);
 global.db = db;
 
 // Setup the session values for storing user logins.
-app.use(session({
+app.use(
+  session({
     secret: 'protoTheta',
     resave: true,
     saveUninitialized: false,
     cookie: {
-        expires: new Date(Date.now() + 3600000),
-        maxAge: 3600000
-    }
-}));
+      expires: new Date(Date.now() + 3600000),
+      maxAge: 3600000,
+    },
+  }),
+);
 
 // Set the express ports, views and public directory names.
 app.use(express.static(__dirname + '/public'));
@@ -52,32 +57,36 @@ app.set('port', process.env.PORT || 3000);
 app.set('view engine', 'ejs');
 
 // Setup the error messaging.
-app.use(function(req, res, next) {
-    res.locals.sessionFlash = req.session.sessionFlash;
-    delete req.session.sessionFlash;
-    next();
+app.use(function (req, res, next) {
+  res.locals.sessionFlash = req.session.sessionFlash;
+  delete req.session.sessionFlash;
+  next();
 });
 
 // Setup the body parser values.
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // Configure the routes to be sent to the router.js file.
 var routes = require('./routes/router');
 app.use('/', routes);
 
 // If the URL entered could not be found, return this message.
-app.use(function(req, res, next) {
-    res.status(404).render('404.ejs', {"loggedIn": req.session.loggedin});
+app.use(function (req, res, next) {
+  res.status(404).render('404.ejs', { loggedIn: req.session.loggedin });
 });
 
 // Response status for error code 500.
-app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.send(err.message);
+app.use(function (err, req, res, next) {
+  res.status(err.status || 500);
+  res.send(err.message);
 });
 
 // Creates the server and listens on the specified port.
-app.listen(app.get('port'), function() {
-    console.log('[Server]: Server has started and is listening on port ' + app.get('port') + '.');
+app.listen(app.get('port'), function () {
+  console.log(
+    '[Server]: Server has started and is listening on port ' +
+      app.get('port') +
+      '.',
+  );
 });
